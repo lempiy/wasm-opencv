06338dc35299bb7209f1ac3a03e19f65b648058b
lempiy <lempiyada@gmail.com>
2017-11-28 01:14:19 +0200

    Come closer to goal

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 let wasmWorker = new Worker('wasm-worker.js');
 
-wasmWorker.onmessage = function (e) {
-    perfwasm1 = performance.now();
-    console.log(`WASM: ${perfwasm1}`, e.data);
-}
-
 window.onload = function() {
     var c=document.getElementById("myCanvas");
+    var co=document.getElementById("outCanvas");
     var ctx=c.getContext("2d");
+    var ctxo=co.getContext("2d");
     var img= new Image();
     img.src = "./pizza.png";
-    console.dir(img)
+
+    wasmWorker.onmessage = function (e) {
+        perfwasm1 = performance.now();
+        console.log(`WASM: ${perfwasm1}`, e.data);
+        if (e.data.msg instanceof ImageData) {
+            console.log(ctx.getImageData(0, 0, c.width, c.height), e.data.msg)
+            console.log()
+            ctxo.putImageData(e.data.msg, 0, 0)
+        }
+    }
     img.onload = function() {
         c.width = img.width
+        co.width = img.width
         c.height = img.height
+        co.height = img.height
         ctx.drawImage(img,0,0);
         setTimeout(function() {
             sendImageData()
         }, 500)
     }
     function sendImageData() {
-        let message = { cmd: 'imageNew', img: ctx.getImageData(0, 0, c.width || 200, c.height || 200)};
+        let message = { cmd: 'imageNew', img: ctx.getImageData(0, 0, c.width, c.height)};
         wasmWorker.postMessage(message);
     }    
 };
--- wasm-worker.js
+++ wasm-worker.js
@@ -14,27 +14,27 @@ setTimeout(function() {
 }, 1000)
 
 function getContours(imageData) {
+    console.log(imageData)
     let tresh = 100;
     let max_tresh = 255;
-    let img = cv.matFromArray(imageData, 24);
-    let img_gray = new cv.Mat();
-    let img_trsh = new cv.Mat();
+    let img = cv.matFromArray(imageData, cv.CV_8UC4);
     let dst = cv.Mat.zeros(img.cols, img.rows, cv.CV_8UC4);
-    console.log(img.size())
-    cv.cvtColor(img, img_gray, cv.ColorConversionCodes.COLOR_RGBA2GRAY.value, 0);
-    console.log(img_gray)
-    cv.threshold(img_gray, img_trsh, 120, 200, cv.ThresholdTypes.THRESH_BINARY.value);
-    console.log(img_trsh)
+    console.log("IMG", cv, img, img.type(), cv.CV_8UC4)
+    console.log(img.type())
+    cv.cvtColor(img, img, cv.ColorConversionCodes.COLOR_RGBA2GRAY.value, 0);
+    console.log(img.type())
+    cv.threshold(img, img, tresh, max_tresh, cv.ThresholdTypes.THRESH_BINARY.value);
+    console.log(img.type())
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
 
-    
-    cv.findContours(img_trsh, contours, hierarchy, cv.RetrievalModes.RETR_CCOMP.value, cv.ContourApproximationModes.CHAIN_APPROX_SIMPLE.value, [0,0])
-    
+    cv.findContours(img, contours, hierarchy, cv.RetrievalModes.RETR_CCOMP.value, cv.ContourApproximationModes.CHAIN_APPROX_SIMPLE.value, [0,0])
     for (let i = 0; i < contours.size(); ++i) {
         let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
-                                  Math.round(Math.random() * 255));
-        console.log(contours.get(i))
+            Math.round(Math.random() * 255), 255);
+        cv.drawContours(dst, contours, i, color, 1, cv.LineTypes.LINE_8.value, hierarchy, 100, [0,0]);
     }
-    img.delete(); img_trsh.delete(); img_gray.delete(); dst.delete(); contours.delete(); hierarchy.delete();
+    console.log(dst.data())
+    postMessage({msg: new ImageData(new Uint8ClampedArray(dst.data()), imageData.width, imageData.height)});
+    img.delete(); dst.delete();contours.delete(); hierarchy.delete();
 }
